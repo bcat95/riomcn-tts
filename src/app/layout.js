285e83f8906981d63a2f0d7ec463b18528ba5590
bcat95 <bcat9505@gmail.com>
2024-07-23 14:14:42 +0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,21 +1,17 @@
-import './globals.css';
-import { Inter } from 'next/font/google';
+import './globals.css'
+import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'RioMCN Văn bản thành Lời nói',
-  description: 'Công cụ chuyển văn bản thành lời nói tài trợ bởi RioMCN',
-};
+    title: 'RioMCN Văn bản thành Lời nói',
+    description: 'Công cụ chuyển văn bản thành lời nói tài trợ bởi RioMCN',
+}
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="vi">
-      <body className={inter.className}>
-        {children}
-        <script defer src="https://chatvn.org/js/rio-tts.js"></script>
-        <div class="tab-pane" id="app-load">
-      </body>
-    </html>
-  );
+    return (
+        <html lang="vi">
+            <body className={inter.className}>{children}</body>
+        </html>
+    )
 }
