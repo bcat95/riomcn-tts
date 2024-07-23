610d856eaace38067ed8895e80d05a36cc1e721c
bcat95 <bcat9505@gmail.com>
2024-07-23 14:03:22 +0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,25 +1,27 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
     title: 'RioMCN Văn bản thành Lời nói',
     description: 'Công cụ chuyển văn bản thành lời nói tài trợ bởi RioMCN',
-}
+};
 
 export default function RootLayout({ children }) {
     return (
         <html lang="vi">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+        {children}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-JFEPJSWCC6"></script>
         <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
+          {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
 
-            gtag('config', 'G-JFEPJSWCC6');
+          gtag('config', 'G-JFEPJSWCC6');`}
         </script>
-        </html>
-    )
+      </body>
+    </html>
+    );
 }
